5d0b85a6ca2163ebb58dbce602b7bbe25c2845c0
Dion Pettaway <dionp@fullsail.edu>
2015-05-16 07:05:12 -0400

    Full search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ActiveRecord::Base
 	def self.search(query)
 		if query.present?
-			where("title like :q", q: "%#{query}%")
-		else
-			all
-		end
-	end
+			rank = <<-RANK
+        ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(query)})) +
+        ts_rank(to_tsvector(url), plainto_tsquery(#{sanitize(query)})) +
+        ts_rank(to_tsvector(content), plainto_tsquery(#{sanitize(query)}))
+      RANK
+      where("title @@ :q or content @@ :q", q: query).order("#{rank} desc")
+    else
+      all
+    end
+  end
 end
